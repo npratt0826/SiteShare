806c200c28adbf1e6f10a7b301e0f8fabe1d8a77
Nick Pratt <nicholasapratt@gmail.com>
2017-11-08 19:49:05 -0500

    refacter incoming control

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,9 +2,6 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
  def create
-
-   puts "INCOMING PARAMS HERE: #{params}"
-
    @user = User.find_by(params[:sender])
    @topic = Topic.find_by(params[:subject])
    @url = params[:body-plain]
@@ -19,6 +16,9 @@ class IncomingController < ApplicationController
 
    @bookmark = @topic.bookmarks.create(url: @url)
 
+   puts "user - #{@user}, topic - #{@topic}, url - #{@url}"
+
    head 200
+
  end
 end
